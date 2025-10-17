a8d7b889a94553e37d54639cb7dd4cf2555a58cc
Jeff Nugkuag <jnugkuago@ucvvirtual.edu.pe>
2025-10-17 01:09:06 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = "https://cnt-11d098b0-3b8f-45e0-a02f-092e7aef4ba9.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
